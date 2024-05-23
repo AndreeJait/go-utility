8a3a68e9f0b96a32bf6b9084e75fee3fce6be93a
Andree Panjaitan <andreepanjaitan@Andrees-MacBook-Pro.local>
2024-05-23 12:51:38 +0700

    request log not shown

--- loggerw/middleware.go
+++ loggerw/middleware.go
@@ -1,8 +1,10 @@
 package loggerw
 
 import (
+	"bytes"
 	"encoding/json"
 	"github.com/labstack/echo/v4"
+	"io"
 )
 
 func LoggerWitRequestID(log Logger, showLog bool) echo.MiddlewareFunc {
@@ -14,15 +16,19 @@ func LoggerWitRequestID(log Logger, showLog bool) echo.MiddlewareFunc {
 			newContext, requestID := WithRequest(ctx, r)
 			r = r.WithContext(newContext)
 
-			if showLog {
-				go func(c echo.Context, log Logger, requestID string) {
+			bodyCopy := new(bytes.Buffer)
+			// Read the whole body
+			_, err := io.Copy(bodyCopy, c.Request().Body)
+
+			if showLog && err != nil {
+				go func(body *bytes.Buffer, log Logger, requestID string) {
 					var mapRequest map[string]interface{}
-					err := c.Bind(&mapRequest)
+					err := json.NewDecoder(bodyCopy).Decode(&mapRequest)
 					if err == nil {
-						bytes, _ := json.Marshal(&mapRequest)
-						log.Infof("[%s] - %s", requestID, string(bytes))
+						marshal, _ := json.Marshal(&mapRequest)
+						log.Infof("[%s] - %s", requestID, string(marshal))
 					}
-				}(c, log, requestID)
+				}(bodyCopy, log, requestID)
 			}
 
 			c.SetRequest(r)
