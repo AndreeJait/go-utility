c4c095822f09e3263552af53a69fdb05ebfec158
Andree Panjaitan <andreepanjaitan@Andrees-MacBook-Pro.local>
2024-12-30 20:45:23 +0700

    update email

---
+++ emailw/dto.go
@@ -0,0 +1,31 @@
+package emailw
+
+type EmailConfig struct {
+	Host     string `json:"host" yaml:"host"`
+	Port     int    `json:"port" yaml:"port"`
+	User     string `json:"user" yaml:"user"`
+	Password string `json:"password" yaml:"password"`
+	UsingSSL bool   `json:"using_t_ls"`
+}
+
+type SentEmailParam struct {
+	Subject     string                 `json:"subject"`
+	Sender      string                 `json:"sender"`
+	To          []string               `json:"to"`
+	Cc          []SentEmailParamCC     `json:"cc"`
+	Message     string                 `json:"message"`
+	Template    string                 `json:"template"`
+	Param       map[string]interface{} `json:"param"`
+	Attachments []SentEmailParamAttach `json:"attachments"`
+}
+
+type SentEmailParamAttach struct {
+	FileName    string `json:"file_name"`
+	FileBase64  string `json:"file_base64"`
+	ContentType string `json:"content_type"`
+}
+
+type SentEmailParamCC struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+}
